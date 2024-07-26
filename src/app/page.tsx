01e9d2e4e8b17da85eb8d0970f9bcea59e4744a1
Sam <samluo1019@gmail.com>
2024-07-26 12:39:52 -0400

    feat: simple typed data sign tool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,213 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Alert,
+  AppBar,
+  Box,
+  Button,
+  Fab,
+  IconButton,
+  Stack,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import DrawIcon from "@mui/icons-material/Draw";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 export default function Home() {
+  const [account, setAccount] = useState<string | null>(null);
+  const [network, setNetwork] = useState<string | null>(null);
+  const [signature, setSignature] = useState<string | null>(null);
+  const [payloadText, setPayloadText] = useState<string>("");
+  const [payloadTextValid, setPayloadTextValid] = useState<boolean>(true);
+  const [payloadTextValidationError, setPayloadTextValidationError] =
+    useState<string>("");
+  const [payloadDataValidation, setPayloadDataValidation] = useState<{
+    types: boolean;
+    domain: boolean;
+    message: boolean;
+  }>({
+    types: true,
+    domain: true,
+    message: true,
+  });
+  const [signingError, setSigningError] = useState<string>("");
+
+  useEffect(() => {
+    setPayloadTextValid(true);
+    setPayloadTextValidationError("");
+    setPayloadDataValidation({
+      types: true,
+      domain: true,
+      message: true,
+    });
+    setSigningError("");
+
+    if (!payloadText) {
+      return;
+    }
+
+    try {
+      const payload = JSON.parse(payloadText);
+      if (!payload.types) {
+        setPayloadDataValidation((prev) => ({ ...prev, types: false }));
+      }
+      if (!payload.domain) {
+        setPayloadDataValidation((prev) => ({ ...prev, domain: false }));
+      }
+      if (!payload.message) {
+        setPayloadDataValidation((prev) => ({ ...prev, message: false }));
+      }
+      setPayloadTextValid(true);
+      setPayloadTextValidationError("");
+    } catch (error) {
+      setPayloadTextValid(false);
+      setPayloadTextValidationError((error as Error).message);
+    }
+  }, [payloadText]);
+
+  const connectMetaMask = async () => {
+    const ethereum = window.ethereum;
+
+    if (typeof ethereum === "undefined") {
+      alert("Please install MetaMask");
+      return;
+    }
+
+    try {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log(accounts);
+
+      const provider = new ethers.BrowserProvider(ethereum);
+      const networkId = await provider.getNetwork();
+
+      setAccount(accounts[0]);
+      setNetwork(networkId.name);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signTypedData = async () => {
+    if (!account) {
+      alert("Please connect MetaMask");
+      return;
+    }
+
+    if (
+      !payloadTextValid ||
+      Object.values(payloadDataValidation).includes(false)
+    ) {
+      alert("Invalid payload");
+      return;
+    }
+
+    setSigningError("");
+
+    const payload = JSON.parse(payloadText);
+    const ethereum = window.ethereum;
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner(account);
+
+    try {
+      const signature = await signer.signTypedData(
+        payload.domain,
+        payload.types,
+        payload.message
+      );
+
+      setSignature(signature);
+    } catch (error) {
+      setSigningError((error as Error).message);
+    }
+  };
+
+  const onPayloadTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPayloadText(event.target.value);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Signature Tools
+          </Typography>
+          {account ? (
+            <Button color="inherit">{`${network}: ${account}`}</Button>
+          ) : (
+            <Button color="inherit" onClick={() => connectMetaMask()}>
+              Connect MetaMask
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Box component="main" sx={{ p: 3 }}>
+        <Stack direction="row" spacing={2}>
+          <Box flex={1}>
+            <TextField
+              label="JSON Payload"
+              multiline
+              fullWidth
+              rows={30}
+              value={payloadText}
+              onChange={onPayloadTextChange}
+              error={!payloadTextValid}
+              helperText={payloadTextValidationError}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </Box>
+          <Box
+            flex={1}
+            sx={{
+              borderRadius: "4px",
+              border: "1px solid #e5e7eb",
+              p: 1,
+            }}
+          >
+            <Stack spacing={2}>
+              {payloadText &&
+                payloadTextValid &&
+                Object.entries(payloadDataValidation).map(([key, value]) => (
+                  <Alert key={key} severity={value ? "success" : "error"}>
+                    {`Payload: ${key} is ${value ? "" : "in"}valid`}
+                  </Alert>
+                ))}
+              {signingError && (
+                <Alert severity="error">{`Signing error: ${signingError}`}</Alert>
+              )}
+              {signature && !signingError && (
+                <Alert severity="success">{`Signature: ${signature}`}</Alert>
+              )}
+            </Stack>
+          </Box>
+        </Stack>
+      </Box>
+      <Box sx={{ position: "absolute", right: 24, bottom: 50 }}>
+        <Fab variant="extended" color="primary" onClick={() => signTypedData()}>
+          <DrawIcon sx={{ mr: 1 }} />
+          Sign Payload
+        </Fab>
+      </Box>
+    </Box>
   );
 }
